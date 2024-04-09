5055a9906dd4c14f71730310753d655f781b1cc0
trying559 <4ohu1xov@anonaddy.com>
2024-04-10 01:08:05 +0200

    vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import {
   Profile,
   Navbar,
@@ -39,6 +40,7 @@ function App() {
         </Routes>
       </div>
       <Footer />
+      <Analytics />
     </Router>
   );
 }
