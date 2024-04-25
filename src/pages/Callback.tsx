3963c6a7ab9fcca36c407c4c6e872b0cbce7f2ba
trying559 <4ohu1xov@anonaddy.com>
2024-04-25 14:32:53 +0200

    Added "Authorization revoked" message if user cancels login, improved visibility on swiper-buttons on episodecards and increased rotation delay.

--- src/pages/Callback.tsx
+++ src/pages/Callback.tsx
@@ -17,15 +17,23 @@ const Callback = () => {
 
   useEffect(() => {
     const queryParams = new URLSearchParams(location.search);
+    const error = queryParams.get('error');
     const code = queryParams.get('code');
     const PLATFORM = import.meta.env.VITE_DEPLOY_PLATFORM; // This will be set as 'VERCEL' or 'CLOUDFLARE'
+
+    // Check if there was an access denied error
+    if (error === 'access_denied') {
+      setErrorMessage(
+        'Authorization revoked. Please click "Authorize" to grant access.',
+      );
+      navigate('/callback', { replace: true });
+      return;
+    }
 
     // Determine the endpoint based on the platform
     const apiEndpoint =
       PLATFORM === 'VERCEL' ? '/api/exchange-token' : '/exchange-token';
 
-    // console.log(apiEndpoint);
-    // console.log(PLATFORM);
     if (code) {
       axios
         .post(apiEndpoint, { code })
@@ -37,8 +45,7 @@ const Callback = () => {
           window.location.reload(); // Force a full page reload to refresh state
         })
         .catch((error) => {
-          const errMsg =
-            error.response?.data?.error || 'An unknown error occurred';
+          const errMsg = error.response?.data?.error || 'Error logging in :(';
           console.error('Error in token exchange:', errMsg);
           setErrorMessage(errMsg); // Store the error message
           navigate('/callback', { replace: true });
@@ -47,9 +54,7 @@ const Callback = () => {
   }, [location, navigate]);
 
   return (
-    <Message>
-      {errorMessage ? `Error: ${errorMessage}` : 'Logging in...'}
-    </Message>
+    <Message>{errorMessage ? `${errorMessage}` : 'Logging in...'}</Message>
   );
 };
 
